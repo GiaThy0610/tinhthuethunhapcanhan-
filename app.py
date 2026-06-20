cf131becdd1e381262560f8a363a003f3e0e3903
GiaThy0610 <dogiathy061003@gmail.com>
2026-06-20 16:16:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def tinh_thue_tncn(gross, bonus, overtime, lunch, other, deps):
             
             temp_income -= taxable_in_bracket
             previous_limit = b["limit"]
-        else:
+    else:
            ' break'
 
     net_salary = total_income - total_insurance - tax
